bfbb6a1998b197313955a8aadf77da9f85d34841
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-01-22 22:30:42 +1100

    Initial commit

---
+++ pass_by_value_parameter/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * C# Program to Demonstrate Pass by Value Parameter 
+ */
+using System;
+
+namespace pass_by_value_parameter
+{
+    class Program
+    {
+        static void Cube(int x)
+        {
+            x = x * x * x;
+            Console.WriteLine("Value Within the Cube method : {0}", x);
+        }
+
+        static void Main()
+        {
+            int num = 5;
+            Console.WriteLine("Value Before the Method is called {0}", num);
+            Cube(num);
+            Console.WriteLine("Value After the Method is called : {0}", num);
+            Console.ReadKey();
+        }
+    }
+}
